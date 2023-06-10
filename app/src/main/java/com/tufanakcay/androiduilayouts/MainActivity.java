b36457e1ddb3af4d944a10319915dd66d0df3800
Tufan AKÇAY <132299647+tufanakcay@users.noreply.github.com>
2023-06-10 19:16:02 +0300

    Update main

--- app/src/main/java/com/tufanakcay/androiduilayouts/MainActivity.java
+++ app/src/main/java/com/tufanakcay/androiduilayouts/MainActivity.java
@@ -3,18 +3,67 @@ package com.tufanakcay.androiduilayouts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    // Button btn;
+    TextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       // setContentView(R.layout.activity_main);
-       // setContentView(R.layout.linearlayout);
-       // setContentView(R.layout.relativelayout);
+        // setContentView(R.layout.activity_main);
+        // setContentView(R.layout.linearlayout);
+        // setContentView(R.layout.relativelayout);
         //setContentView(R.layout.linearlayout_menu);
         //setContentView(R.layout.relative_layout);
-        setContentView(R.layout.layout_button);
+        //setContentView(R.layout.layout_button);
+        setContentView(R.layout.layout_textview);
+
+        tanimla();
+
+        goster("Hello world!");
+
+        //  isVer();
 
     }
+
+    private void tanimla() {
+
+        //   btn = findViewById(R.id.button001);
+        textView = findViewById(R.id.textview);
+
+    }
+
+
+    private void goster(String isimVer) {
+
+        textView.setText(isimVer);
+
+
+    }
+
+    /*
+    private void isVer() {
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                System.out.println("Button clicked");
+                Toast.makeText(MainActivity.this, "Button clicked", Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+     */
+
+
 }
